92cf7d7730e5d61d6e4b1a5649727da2c3993481
Kagwi <kelvinmwas8@gmail.com>
2025-01-31 22:33:19 +0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ const features = [
     icon: <Award className="h-8 w-8 text-yellow-400" />,
     title: "Premium Quality",
     description: "Our honey is harvested with care to maintain its pure, natural qualities",
-    image: "https://images.unsplash.com/photo-1607342283552-0ba28b724d6a?crop=entropy&cs=tinysrgb&fit=max&ixid=MXwyMDg1MnwwfDF8c2VhcmNofDJ8fG1vbmV5fGVufDB8fHx8&ixlib=rb-1.2.1&q=80&w=400",
+    image: "https://github.com/Kagwi/Nyuki-Haven/blob/main/Honey%20Jar%20Background.jpg?raw=true&fit=max&ixid=MXwyMDg1MnwwfDF8c2VhcmNofDJ8fG1vbmV5fGVufDB8fHx8&ixlib=rb-1.2.1&q=80&w=400",
   },
   {
     icon: <Shield className="h-8 w-8 text-yellow-400" />,
