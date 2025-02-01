228d1c27c6c3622c999d659df365565b4a155ea8
Kagwi <kelvinmwas8@gmail.com>
2025-02-01 07:58:00 +0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,18 +9,18 @@ export default function Header() {
   const isActive = (path) => location.pathname === path;
 
   return (
-    <header className="bg-purple-900 text-white py-2 px-4 shadow-lg">
+    <header className="bg-purple-900 text-white py-1 px-3 shadow-lg">
       <div className="container mx-auto flex flex-col md:flex-row items-center justify-between">
         <div className="flex items-center justify-between w-full md:w-auto">
           <div className="flex items-center group">
             <img 
               src="https://github.com/Kagwi/Nyuki-Haven/blob/main/Nyuki_Haven_Logo.png?raw=true" 
               alt="Nyuki Haven Logo" 
-              className="h-20 w-20 md:h-24 md:w-24 mr-4 group-hover:rotate-12 transition-transform object-contain"
+              className="h-20 w-20 md:h-24 md:w-24 mr-3 group-hover:rotate-12 transition-transform object-contain"
             />
             <div>
-              <Link to="/" className="text-2xl font-bold group-hover:text-yellow-400 transition-colors">Nyuki Haven</Link>
-              <p className="text-base text-yellow-400 group-hover:text-white transition-colors">Nurturing Bees. Sustaining Life</p>
+              <Link to="/" className="text-xl font-bold group-hover:text-yellow-400 transition-colors">Nyuki Haven</Link>
+              <p className="text-sm text-yellow-400 group-hover:text-white transition-colors">Nurturing Bees. Sustaining Life</p>
             </div>
           </div>
           <button 
@@ -30,8 +30,8 @@ export default function Header() {
             {isMenuOpen ? <X className="h-6 w-6" /> : <Menu className="h-6 w-6" />}
           </button>
         </div>
-        <nav className={`${isMenuOpen ? 'block' : 'hidden'} md:block w-full md:w-auto mt-3 md:mt-0`}>
-          <ul className="flex flex-col md:flex-row space-y-1 md:space-y-0 md:space-x-4">
+        <nav className={`${isMenuOpen ? 'block' : 'hidden'} md:block w-full md:w-auto mt-2 md:mt-0`}>
+          <ul className="flex flex-col md:flex-row space-y-1 md:space-y-0 md:space-x-3">
             <li>
               <Link 
                 to="/" 
